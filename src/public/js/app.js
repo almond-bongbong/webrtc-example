96d418b1796159c92bb52f3975fd64d2157be7af
cmlee <bal.dongdongdong@gmail.com>
2021-11-12 20:41:21 +0900

    (feat) set nickname part1

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -1,6 +1,13 @@
 const socket = new WebSocket(`ws://${window.location.host}`);
 const messageList = document.querySelector('ul');
-const messageForm = document.querySelector('form');
+const nickForm = document.querySelector('#nick');
+const messageForm = document.querySelector('#message');
+
+const makeMessage = (type, payload) =>
+  JSON.stringify({
+    type,
+    payload,
+  });
 
 socket.addEventListener('open', () => console.log('Connected to Server ✅'));
 
@@ -9,7 +16,9 @@ socket.addEventListener('close', () =>
 );
 
 socket.addEventListener('message', (message) => {
-  messageList.append(message.data);
+  const li = document.createElement('li');
+  li.innerText = message.data;
+  messageList.append(li);
 });
 
 setTimeout(() => {
@@ -19,6 +28,12 @@ setTimeout(() => {
 messageForm.addEventListener('submit', (e) => {
   e.preventDefault();
   const input = messageForm.querySelector('input');
-  socket.send(input.value);
+  socket.send(makeMessage('message', input.value));
   input.value = '';
+});
+
+nickForm.addEventListener('submit', (e) => {
+  e.preventDefault();
+  const input = nickForm.querySelector('input');
+  socket.send(makeMessage('nickname', input.value));
 });
